682901574dc99234b670f8ee05e429f11fa41143
Your Name <you@example.com>
2024-04-19 17:33:00 -0400

    3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -66,6 +66,15 @@ app.get('/api/persons/:id', (request, response) => {
       response.status(404).end()
   }
 })
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+
+   //Filter out the matched id
+   persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
   
 const PORT = 3001
 app.listen(PORT, () => {
