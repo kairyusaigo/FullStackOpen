3a8cda2ecbd2a04accd1c9ef549a3796bff1db2c
Your Name <you@example.com>
2024-04-20 00:28:16 -0400

    3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,11 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   { 
