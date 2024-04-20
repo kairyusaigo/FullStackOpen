211c2fa5696f2b4cad242f0a07b6e0cf6d3153d4
Your Name <you@example.com>
2024-04-19 21:53:20 -0400

    3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -76,7 +76,20 @@ function getRandomInt(min, max) {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number is missing' 
+    })
+  }
+
+  const isFound = persons.some(person => person.name === body.name)
+  if (isFound) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
   const person = {
     name: body.name,
     number: body.number,
