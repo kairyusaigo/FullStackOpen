c7b0384c21c6066cbc47e83e84554878ab1bbd84
Your Name <you@example.com>
2024-05-02 18:56:40 -0400

    4.4

--- part4/blogs/test/dummy.test.js
+++ part4/blogs/test/dummy.test.js
@@ -8,3 +8,59 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithMultiBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '66303f043c8a925aa81e343b',
+      title: 'The Future of AI: Trends and Innovations to Watch in 2024',
+      author: 'Dave Bergmann',
+      url: 'https://www.ibm.com/blog/artificial-intelligence-trends',
+      likes: 17,
+      __v: 0
+    },
+    {
+      _id: '66303f453c8a925aa81e343e',
+      title: 'For the planet',
+      author: 'Johnny H.',
+      url: 'https://www.planet.com',
+      likes: 21,
+      __v: 0
+    }
+  ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listWithMultiBlog)
+    assert.strictEqual(result, 43)
+  })
+
+})
