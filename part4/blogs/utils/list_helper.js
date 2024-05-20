b7c138a5bc78b092155640e90532671dfe9b1852
Your Name <you@example.com>
2024-05-20 12:19:22 -0400

    4.7

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -18,6 +18,40 @@ const favoriteBlog = (blogs) => {
   return blogTrim[blogTrim.indexOf(maxLikes)]
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return {}
+
+  const blogCounts = blogs
+    .reduce((blogCount, blog) => {
+      blogCount[blog.author] = (blogCount[blog.author] || 0) +1
+      return blogCount
+    }, {})
+
+  const authorSort = Object.entries(blogCounts).sort((a,b) => b[1]-a[1])
+
+  return {
+    author: authorSort[0][0],
+    blogs: authorSort[0][1]
+  }
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return {}
+
+  const likeCounts = blogs
+    .reduce((likeCount, blog) => {
+      likeCount[blog.author] = (likeCount[blog.author] || 0) + blog.likes
+      return likeCount
+    }, {})
+
+  const authorSort = Object.entries(likeCounts).sort((a,b) => b[1]-a[1])
+
+  return {
+    author: authorSort[0][0],
+    likes: authorSort[0][1]
+  }
+}
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
